98bc170f6773fb03ed4f506a3a14b04ccd9e356e
TiagoSantosDev <up201808919@fe.up.pt>
2021-03-10 15:00:29 +0000

    Added intent to button on date picker screen.

--- app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
+++ app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -41,6 +42,12 @@ class DatePickerActivity : BaseActivity() {
                     initAdapter()
                 }
             })
+        }
+
+        val button = findViewById<Button>(R.id.button_escolher)
+        button.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
         mAttributes.onOrientationChanged = { oldValue, newValue ->
