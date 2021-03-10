07214e6c88f66402c30af4a44b90a9cf25c4201d
TiagoSantosDev <up201808919@fe.up.pt>
2021-03-10 15:07:38 +0000

    Added new main activity

--- app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
+++ app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
@@ -23,26 +23,9 @@ import java.util.ArrayList
 
 class DatePickerActivity : BaseActivity() {
 
-    private val mDataList = ArrayList<TimeLineModel>()
-    private lateinit var mLayoutManager: LinearLayoutManager
-    private lateinit var mAttributes: TimelineAttributes
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentViewWithoutInject(R.layout.activity_date_picker)
-
-        initRecyclerView()
-
-        action_example_activity.setOnClickListener { startActivity(Intent(this, ExampleActivity::class.java)) }
-
-        fab_options.setOnClickListener {
-            TimelineAttributesBottomSheet.showDialog(supportFragmentManager, mAttributes, object: TimelineAttributesBottomSheet.Callbacks {
-                override fun onAttributesChanged(attributes: TimelineAttributes) {
-                    mAttributes = attributes
-                    initAdapter()
-                }
-            })
-        }
 
         val button = findViewById<Button>(R.id.button_escolher)
         button.setOnClickListener{
@@ -50,37 +33,7 @@ class DatePickerActivity : BaseActivity() {
             startActivity(intent)
         }
 
-        mAttributes.onOrientationChanged = { oldValue, newValue ->
-            if(oldValue != newValue) initRecyclerView()
-        }
-
-        mAttributes.orientation = Orientation.VERTICAL
     }
 
-
-
-    private fun initRecyclerView() {
-        initAdapter()
-        recyclerView.addOnScrollListener(object: RecyclerView.OnScrollListener() {
-            @SuppressLint("LongLogTag")
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                if(recyclerView.getChildAt(0).top < 0) dropshadow.setVisible() else dropshadow.setGone()
-            }
-        })
-    }
-
-    private fun initAdapter() {
-        mLayoutManager = if (mAttributes.orientation == Orientation.HORIZONTAL) {
-            LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
-        } else {
-            LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        }
-
-        recyclerView.apply {
-            layoutManager = mLayoutManager
-            adapter =  TimeLineAdapter(mDataList, mAttributes)
-        }
-    }
 
 }
