3264eb7a029a04d1b7bd6e153134459d0b5b3f7c
TiagoSantosDev <up201808919@fe.up.pt>
2021-03-10 14:56:02 +0000

    Added date picker Screen.

---
+++ app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
@@ -0,0 +1,79 @@
+package com.github.vipulasri.timelineview.sample
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.vipulasri.timelineview.TimelineView
+import com.github.vipulasri.timelineview.sample.example.ExampleActivity
+import com.github.vipulasri.timelineview.sample.extentions.dpToPx
+import com.github.vipulasri.timelineview.sample.extentions.getColorCompat
+import com.github.vipulasri.timelineview.sample.extentions.setGone
+import com.github.vipulasri.timelineview.sample.extentions.setVisible
+import com.github.vipulasri.timelineview.sample.model.OrderStatus
+import com.github.vipulasri.timelineview.sample.model.Orientation
+import com.github.vipulasri.timelineview.sample.model.TimeLineModel
+import com.github.vipulasri.timelineview.sample.model.TimelineAttributes
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+
+class DatePickerActivity : BaseActivity() {
+
+    private val mDataList = ArrayList<TimeLineModel>()
+    private lateinit var mLayoutManager: LinearLayoutManager
+    private lateinit var mAttributes: TimelineAttributes
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentViewWithoutInject(R.layout.activity_date_picker)
+
+        initRecyclerView()
+
+        action_example_activity.setOnClickListener { startActivity(Intent(this, ExampleActivity::class.java)) }
+
+        fab_options.setOnClickListener {
+            TimelineAttributesBottomSheet.showDialog(supportFragmentManager, mAttributes, object: TimelineAttributesBottomSheet.Callbacks {
+                override fun onAttributesChanged(attributes: TimelineAttributes) {
+                    mAttributes = attributes
+                    initAdapter()
+                }
+            })
+        }
+
+        mAttributes.onOrientationChanged = { oldValue, newValue ->
+            if(oldValue != newValue) initRecyclerView()
+        }
+
+        mAttributes.orientation = Orientation.VERTICAL
+    }
+
+
+
+    private fun initRecyclerView() {
+        initAdapter()
+        recyclerView.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            @SuppressLint("LongLogTag")
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if(recyclerView.getChildAt(0).top < 0) dropshadow.setVisible() else dropshadow.setGone()
+            }
+        })
+    }
+
+    private fun initAdapter() {
+        mLayoutManager = if (mAttributes.orientation == Orientation.HORIZONTAL) {
+            LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        } else {
+            LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        }
+
+        recyclerView.apply {
+            layoutManager = mLayoutManager
+            adapter =  TimeLineAdapter(mDataList, mAttributes)
+        }
+    }
+
+}
