c1d842c8fb97b4f6ff503b5c87f16291b2f0d0c2
TiagoSantosDev <up201808919@fe.up.pt>
2021-03-11 11:53:57 +0000

    Fixed errors

--- app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
+++ app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
@@ -8,14 +8,16 @@ import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
 import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
 import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
 import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
+import com.michalsvec.singlerowcalendar.utils.DateUtils
 import kotlinx.android.synthetic.main.activity_date_picker.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.calendar_item.view.*
 import java.util.*
 
 class DatePickerActivity : BaseActivity() {
 
     private val calendar = Calendar.getInstance()
-    private val currentMonth = 0
+    private var currentMonth = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +32,6 @@ class DatePickerActivity : BaseActivity() {
             startActivity(intent)
         }
 
-        // calendar view manager is responsible for our displaying logic
         val myCalendarViewManager = object : CalendarViewManager {
             override fun setCalendarViewResourceId(
                     position: Int,
@@ -45,20 +46,23 @@ class DatePickerActivity : BaseActivity() {
                 // will be using basic item view
                 return if (isSelected)
                     when (cal[Calendar.DAY_OF_WEEK]) {
-                        //R.layout.selected_calendar_item
-
+                        Calendar.MONDAY -> R.layout.first_special_selected_calendar_item
+                        Calendar.WEDNESDAY -> R.layout.second_special_selected_calendar_item
+                        Calendar.FRIDAY -> R.layout.third_special_selected_calendar_item
+                        else -> R.layout.selected_calendar_item
                     }
                 else
                 // here we return items which are not selected
                     when (cal[Calendar.DAY_OF_WEEK]) {
-                        //R.layout.calendar_item
+                        Calendar.MONDAY -> R.layout.first_special_calendar_item
+                        Calendar.WEDNESDAY -> R.layout.second_special_calendar_item
+                        Calendar.FRIDAY -> R.layout.third_special_calendar_item
+                        else -> R.layout.calendar_item
                     }
 
                 // NOTE: if we don't want to do it this way, we can simply change color of background
                 // in bindDataToCalendarView method
             }
-
-
 
             override fun bindDataToCalendarView(
                     holder: SingleRowCalendarAdapter.CalendarViewHolder,
@@ -66,42 +70,38 @@ class DatePickerActivity : BaseActivity() {
                     position: Int,
                     isSelected: Boolean
             ) {
-                // bind data to calendar item views
+                // using this method we can bind data to calendar view
+                // good practice is if all views in layout have same IDs in all item views
+                holder.itemView.tv_date_calendar_item.text = DateUtils.getDayNumber(date)
+                holder.itemView.tv_day_calendar_item.text = DateUtils.getDay3LettersName(date)
 
-            }
-
-        }
-
-
-        val mySelectionManager = object : CalendarSelectionManager {
-            override fun canBeItemSelected(position: Int, date: Date): Boolean {
-                // return true if item can be selected
-                return true;
             }
         }
 
 
+        // using calendar changes observer we can track changes in calendar
         val myCalendarChangesObserver = object : CalendarChangesObserver {
-            override fun whenWeekMonthYearChanged(weekNumber: String, monthNumber: String, monthName: String, year: String, date: Date) {
-                super.whenWeekMonthYearChanged(weekNumber, monthNumber, monthName, year, date)
-            }
-
+            // you can override more methods, in this example we need only this one
             override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
+                tvDate.text = "${DateUtils.getMonthName(date)}, ${DateUtils.getDayNumber(date)} "
+                tvDay.text = DateUtils.getDayName(date)
                 super.whenSelectionChanged(isSelected, position, date)
             }
+        }
 
-            override fun whenCalendarScrolled(dx: Int, dy: Int) {
-                super.whenCalendarScrolled(dx, dy)
+        // selection manager is responsible for managing selection
+        val mySelectionManager = object : CalendarSelectionManager {
+            override fun canBeItemSelected(position: Int, date: Date): Boolean {
+                // set date to calendar according to position
+                val cal = Calendar.getInstance()
+                cal.time = date
+                //in this example sunday and saturday can't be selected, other item can be selected
+                return when (cal[Calendar.DAY_OF_WEEK]) {
+                    Calendar.SATURDAY -> false
+                    Calendar.SUNDAY -> false
+                    else -> true
+                }
             }
-
-            override fun whenSelectionRestored() {
-                super.whenSelectionRestored()
-            }
-
-            override fun whenSelectionRefreshed() {
-                super.whenSelectionRefreshed()
-            }
-
         }
 
         val singleRowCalendar = main_single_row_calendar.apply {
@@ -115,6 +115,33 @@ class DatePickerActivity : BaseActivity() {
 
 
     }
+
+    private fun getDatesOfNextMonth(): List<Date> {
+        currentMonth++ // + because we want next month
+        if (currentMonth == 12) {
+            // we will switch to january of next year, when we reach last month of year
+            calendar.set(Calendar.YEAR, calendar[Calendar.YEAR] + 1)
+            currentMonth = 0 // 0 == january
+        }
+        return getDates(mutableListOf())
+    }
+
+    private fun getDatesOfPreviousMonth(): List<Date> {
+        currentMonth-- // - because we want previous month
+        if (currentMonth == -1) {
+            // we will switch to december of previous year, when we reach first month of year
+            calendar.set(Calendar.YEAR, calendar[Calendar.YEAR] - 1)
+            currentMonth = 11 // 11 == december
+        }
+        return getDates(mutableListOf())
+    }
+
+    private fun getFutureDatesOfCurrentMonth(): List<Date> {
+        // get all next dates of current month
+        currentMonth = calendar[Calendar.MONTH]
+        return getDates(mutableListOf())
+    }
+
 
     private fun getDates(list: MutableList<Date>): List<Date> {
         // load dates of whole month
