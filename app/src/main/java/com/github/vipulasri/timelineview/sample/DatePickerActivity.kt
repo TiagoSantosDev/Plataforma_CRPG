66cab1f21a45017b0fb6ed782a646ca3d62ca73a
TiagoSantosDev <up201808919@fe.up.pt>
2021-03-11 10:53:00 +0000

    Backup date picker XML

--- app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
+++ app/src/main/java/com/github/vipulasri/timelineview/sample/DatePickerActivity.kt
@@ -18,8 +18,13 @@ import com.github.vipulasri.timelineview.sample.model.OrderStatus
 import com.github.vipulasri.timelineview.sample.model.Orientation
 import com.github.vipulasri.timelineview.sample.model.TimeLineModel
 import com.github.vipulasri.timelineview.sample.model.TimelineAttributes
+import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
+import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
+import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
+import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
+import kotlinx.android.synthetic.main.activity_date_picker.*
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.ArrayList
+import java.util.*
 
 class DatePickerActivity : BaseActivity() {
 
@@ -28,12 +33,75 @@ class DatePickerActivity : BaseActivity() {
         setContentViewWithoutInject(R.layout.activity_date_picker)
 
         val button = findViewById<Button>(R.id.button_escolher)
-        button.setOnClickListener{
+        button.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
+        val myCalendarViewManager = object : CalendarViewManager {
+            override fun setCalendarViewResourceId(
+                    position: Int,
+                    date: Date,
+                    isSelected: Boolean
+            ): Int {
+                // return item layout files, which you have created
+                return 1;
+            }
+
+            override fun bindDataToCalendarView(
+                    holder: SingleRowCalendarAdapter.CalendarViewHolder,
+                    date: Date,
+                    position: Int,
+                    isSelected: Boolean
+            ) {
+                // bind data to calendar item views
+            }
+
+        }
+
+
+        val mySelectionManager = object : CalendarSelectionManager {
+            override fun canBeItemSelected(position: Int, date: Date): Boolean {
+                // return true if item can be selected
+                return true;
+            }
+        }
+
+
+        val myCalendarChangesObserver = object : CalendarChangesObserver {
+            override fun whenWeekMonthYearChanged(weekNumber: String, monthNumber: String, monthName: String, year: String, date: Date) {
+                super.whenWeekMonthYearChanged(weekNumber, monthNumber, monthName, year, date)
+            }
+
+            override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
+                super.whenSelectionChanged(isSelected, position, date)
+            }
+
+            override fun whenCalendarScrolled(dx: Int, dy: Int) {
+                super.whenCalendarScrolled(dx, dy)
+            }
+
+            override fun whenSelectionRestored() {
+                super.whenSelectionRestored()
+            }
+
+            override fun whenSelectionRefreshed() {
+                super.whenSelectionRefreshed()
+            }
+
+        }
+
+        val singleRowCalendar = main_single_row_calendar.apply {
+            calendarViewManager = myCalendarViewManager
+            calendarChangesObserver = myCalendarChangesObserver
+            calendarSelectionManager = mySelectionManager
+            futureDaysCount = 30
+            includeCurrentDate = true
+            init()
+        }
+
+
     }
 
-
 }
+
