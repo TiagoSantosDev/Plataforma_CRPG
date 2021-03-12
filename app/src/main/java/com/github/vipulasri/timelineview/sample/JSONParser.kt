128c3e2eada760f7b33a24617d43e3d64c04dd56
TiagoSantosDev <up201808919@fe.up.pt>
2021-03-12 18:15:35 +0000

    Changed JSONParser file

--- app/src/main/java/com/github/vipulasri/timelineview/sample/JSONParser.kt
+++ app/src/main/java/com/github/vipulasri/timelineview/sample/JSONParser.kt
@@ -4,13 +4,16 @@ import android.R.attr.data
 import androidx.appcompat.app.AppCompatActivity
 import com.github.vipulasri.timelineview.sample.model.Meal
 import com.google.gson.Gson
+import java.io.File
 
 class JSONParser : AppCompatActivity() {
 
     val meal = Meal("almondegas", "sardinhas", "sopa", "tofu")
 
+    val file = File("meal")
+
+    // file.writeText(jsonString)
+
     var gson = Gson()
     var json = gson.toJson(data)
-
-
 }
