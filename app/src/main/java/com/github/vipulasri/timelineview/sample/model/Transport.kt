071371a59699fef6218ea52c59077f90c046dcd3
TiagoSantosDev <up201808919@fe.up.pt>
2021-03-15 15:45:11 +0000

    Added Transport to model

---
+++ app/src/main/java/com/github/vipulasri/timelineview/sample/model/Transport.kt
@@ -0,0 +1,8 @@
+package com.github.vipulasri.timelineview.sample.model
+
+data class Transport(
+    val name: String,
+    val description: String,
+    val start_time: String,
+    val end_time: String
+)
