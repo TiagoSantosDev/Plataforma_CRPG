ca7855e70eb850b6f181fac4d1d1024ce147dc8d
TiagoSantosDev <up201808919@fe.up.pt>
2021-04-04 00:14:40 +0100

    Refactor and code cleanup

--- app/src/main/java/com/plataforma/crpg/model/Event.kt
+++ app/src/main/java/com/plataforma/crpg/model/Event.kt
@@ -1,9 +1,25 @@
 package com.plataforma.crpg.model
 
-data class Event(
-    val title: String,
-    val description: String,
-    val type: String,
-    val start_time: String,
-    val end_time: String
-)
+data class EventModel(
+        var title: String,
+        val info: String,
+        var start_time: String,
+        val end_time: String,
+        var date: String,
+        val notas: String,
+        var alarm_type: AlarmType,
+        var alarm_freq: AlarmFrequency
+){
+    override fun toString(): String {
+        return "title: ${this.title}, info: ${this.info}, start_time: ${this.start_time}, " +
+                "end_time: ${this.end_time}, date: ${this.date}"
+    }
+}
+
+enum class AlarmType {
+    SOM, VIBRAR, AMBOS
+}
+
+enum class AlarmFrequency {
+    HOJE, AMANHA, TODOS_OS_DIAS, PERSONALIZADO
+}
