529a16e21f5c4ab985e125a562acaef96978b59f
TiagoSantosDev <up201808919@fe.up.pt>
2021-04-07 14:46:20 +0100

    Resized visual elements

--- app/src/main/java/com/plataforma/crpg/ui/transports/TransportsViewModel.kt
+++ app/src/main/java/com/plataforma/crpg/ui/transports/TransportsViewModel.kt
@@ -2,22 +2,23 @@ package com.plataforma.crpg.ui.transports
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.app.PendingIntent.getActivity
-import android.content.Context
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.plataforma.crpg.model.Meal
-import java.io.File
-import java.io.FileOutputStream
+import java.util.ArrayList
 
 @SuppressLint("StaticFieldLeak")
 class TransportsViewModel(application: Application) : AndroidViewModel(application) {
 
 
-    lateinit var nome_motorista: String
+    var nome_motorista_para_CRPG = "Jorge"
+    var nome_motorista_de_CRPG = "Eliseu"
     private val context = getApplication<Application>().applicationContext
+
+    fun extractDataFromTransportModel(): ArrayList<String> {
+
+
+
+        return ArrayList()
+    }
 
 
 }
