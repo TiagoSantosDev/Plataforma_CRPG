f9db6ac7d4e45ed6641fb662d0f2104314c84602
TiagoSantosDev <up201808919@fe.up.pt>
2021-04-09 15:42:14 +0100

    Added new Text Note Layout

--- app/src/main/java/com/plataforma/crpg/ui/notes/NewVoiceNoteFragment.kt
+++ app/src/main/java/com/plataforma/crpg/ui/notes/NewVoiceNoteFragment.kt
@@ -1,9 +1,13 @@
 package com.plataforma.crpg.ui.notes
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +16,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
+import com.github.windsekirun.naraeaudiorecorder.NaraeAudioRecorder
+import com.github.windsekirun.naraeaudiorecorder.config.AudioRecordConfig
+import com.github.windsekirun.naraeaudiorecorder.source.NoiseAudioSource
 import com.plataforma.crpg.R
 import com.plataforma.crpg.databinding.NotesFragmentBinding
 import kotlinx.android.synthetic.main.fragment_date_picker.*
 import kotlinx.android.synthetic.main.transports_fragment.*
+import java.io.File
 
 
 class NewVoiceNoteFragment : Fragment(), AdapterView.OnItemSelectedListener {
@@ -40,8 +48,45 @@ class NewVoiceNoteFragment : Fragment(), AdapterView.OnItemSelectedListener {
         super.onActivityCreated(savedInstanceState)
         notesViewModel = ViewModelProvider(this).get(NotesViewModel::class.java)
 
+        val audioRecorder = NaraeAudioRecorder()
+        val destFile = File(Environment.getExternalStorageDirectory(), "/NaraeAudioRecorder/$fileName$extensions")
+        audioRecorder.create() {
+            this.destFile = destFile
+        }
+
+        val recordConfig = AudioRecordConfig.defaultConfig()
+        val audioSource = NoiseAudioSource(recordConfig)
+        audioRecorder.create() {
+            this.destFile = this.destFile
+            this.recordConfig = recordConfig
+            this.audioSource = audioSource
+        }
+
+        button_start_recording.setOnClickListener {
+            audioRecorder.startRecording(requireActivity())
+        }
+
+        button_stop_recording.setOnClickListener {
+            audioRecorder.stopRecording()
+        }
 
 
+        button_play_recording.setOnClickListener {
+
+            val myUri: Uri = Environment.getExternalStorageDirectory().absolutePath // initialize Uri here
+            val mediaPlayer = MediaPlayer().apply {
+                setAudioAttributes(
+                        AudioAttributes.Builder()
+                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                .setUsage(AudioAttributes.USAGE_MEDIA)
+                                .build()
+                )
+                setDataSource(activity, myUri)
+                prepare()
+                start()
+            }
+
+        }
 
 
 
