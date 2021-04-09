b2ae8352af23987122048dacfa673a06f44b5296
TiagoSantosDev <up201808919@fe.up.pt>
2021-04-09 15:12:31 +0100

    Experimenting with audio recording

--- app/src/main/java/com/plataforma/crpg/ui/notes/NotesViewModel.kt
+++ app/src/main/java/com/plataforma/crpg/ui/notes/NotesViewModel.kt
@@ -3,21 +3,29 @@ package com.plataforma.crpg.ui.notes
 import android.annotation.SuppressLint
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.plataforma.crpg.model.EventModel
+import com.plataforma.crpg.model.Note
 import java.util.ArrayList
 
 @SuppressLint("StaticFieldLeak")
 class NotesViewModel(application: Application) : AndroidViewModel(application) {
 
-
-    var nome_motorista_para_CRPG = "Jorge"
-    var nome_motorista_de_CRPG = "Eliseu"
     private val context = getApplication<Application>().applicationContext
+    var mReminderList = ArrayList<Note>()
+    lateinit var newNote : Note
 
-    fun extractDataFromTransportModel(): ArrayList<String> {
+
+    fun addNewVoiceNote() {
 
 
 
-        return ArrayList()
+    }
+
+
+    fun addNewTextNote() {
+
+
+
     }
 
 
