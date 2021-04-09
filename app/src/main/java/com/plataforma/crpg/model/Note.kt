756c64059c21d6dfa6a0b9a49fb506c5b4d37081
TiagoSantosDev <up201808919@fe.up.pt>
2021-04-09 16:42:58 +0100

    Added more elements to new text note layout

--- app/src/main/java/com/plataforma/crpg/model/Note.kt
+++ app/src/main/java/com/plataforma/crpg/model/Note.kt
@@ -1,12 +1,14 @@
 package com.plataforma.crpg.model
 
+import android.graphics.Bitmap
+
 data class Note(
     val data: String,
     val hora: String,
     val tipo: NoteType,
     val titulo: String,
     val info: String,
-
+    val imagem: Bitmap
 ){
     override fun toString(): String {
         return "data: ${this.data},hora: ${this.hora}, titulo: ${this.titulo}, info: ${this.info}"
