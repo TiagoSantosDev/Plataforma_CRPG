54c728f4c7c0caaad3e2bbe36e10eedf5a2e6c58
TiagoSantosDev <up201808919@fe.up.pt>
2021-04-21 18:07:33 +0100

    Repositioned elements in Meditation Screen.

---
+++ app/src/main/java/com/plataforma/crpg/ui/meditation/MeditationFragment.kt
@@ -0,0 +1,49 @@
+package com.plataforma.crpg.ui.meditation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.plataforma.crpg.R
+import kotlinx.android.synthetic.main.notes_fragment.*
+
+class MeditationFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MeditationFragment()
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?,
+    ): View? {
+        val binding = MeditationFragment.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.title = "MEDITAÇÃO"
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_new_voice_note.setOnClickListener {
+            val fragment: Fragment = MeditationMediaPlayerFragment()
+            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+            fragmentManager.popBackStack()
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+    }
+
+}
