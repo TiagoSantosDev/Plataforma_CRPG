76d938b4fd458143ee50b2ca655d3496eb4d2dce
TiagoSantosDev <up201808919@fe.up.pt>
2021-05-06 13:46:25 +0100

    Adding color to pressed buttons on Reminders screen.

---
+++ app/src/main/java/com/plataforma/crpg/ui/transports/TransportsSelectionFragment.kt
@@ -0,0 +1,97 @@
+package com.plataforma.crpg.ui.transports
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
+import com.plataforma.crpg.R
+import com.plataforma.crpg.databinding.TransportsFragmentBinding
+import com.plataforma.crpg.ui.MainActivity
+import com.plataforma.crpg.ui.agenda.AgendaFragment
+import com.plataforma.crpg.ui.agenda.SharedViewModel
+import kotlinx.android.synthetic.main.transports_fragment.*
+
+
+class TransportsSelectionFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TransportsSelectionFragment()
+    }
+
+    private lateinit var transportsViewModel: TransportsViewModel
+    private lateinit var sharedViewModel: SharedViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?,
+    ): View {
+        val binding = TransportsFragmentBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        showBackButton()
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Handle the back button event
+                val fragment: Fragment = AgendaFragment()
+                val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+                val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+                fragmentManager.popBackStack()
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackPressedCallback)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.title = "SELEÇÃO DE TRANSPORTES"
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        sharedViewModel = ViewModelProvider(activity as AppCompatActivity).get(SharedViewModel::class.java)
+        transportsViewModel = ViewModelProvider(this).get(TransportsViewModel::class.java)
+
+        button_consult_custom_transport.setOnClickListener {
+            val fragment: Fragment = CustomTransportsFragment()
+            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+            fragmentManager.popBackStack()
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        button_consult_public_transport.setOnClickListener {
+            val fragment: Fragment = PublicTransportsFragment()
+            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+            fragmentManager.popBackStack()
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+    }
+
+    private fun showBackButton() {
+        if (activity is MainActivity) {
+            (activity as MainActivity?)?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+}
+//println("Item selecionado: $selItem")
