9ff649433031a19c515acd2ddc153150be34356e
TiagoSantosDev <up201808919@fe.up.pt>
2021-05-13 15:51:54 +0100

    Moved Unit Tests to test folder (Android Test folder can be used for the UI testing).

---
+++ app/src/test/java/com/plataforma/crpg/ui/reminders/ReminderViewModelTest.kt
@@ -0,0 +1,72 @@
+package com.plataforma.crpg.ui.reminders
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ReminderViewModelTest {
+
+    @Test
+    fun getWeekDaysBoolean() {
+    }
+
+    @Test
+    fun getNewReminder() {
+    }
+
+    @Test
+    fun setNewReminder() {
+    }
+
+    @Test
+    fun getStartTimeHours() {
+    }
+
+    @Test
+    fun setStartTimeHours() {
+    }
+
+    @Test
+    fun getStartTimeMin() {
+    }
+
+    @Test
+    fun setStartTimeMin() {
+    }
+
+    @Test
+    fun getAlarmIntent() {
+    }
+
+    @Test
+    fun setAlarmIntent() {
+    }
+
+    @Test
+    fun getFlagReminderAdded() {
+    }
+
+    @Test
+    fun setFlagReminderAdded() {
+    }
+
+    @Test
+    fun retrieveListReminders() {
+    }
+
+    @Test
+    fun addReminder() {
+    }
+
+    @Test
+    fun setAlarmVibrateOnly() {
+    }
+
+    @Test
+    fun setAlarmBoth() {
+    }
+
+    @Test
+    fun setAlarmSoundOnly() {
+    }
+}
