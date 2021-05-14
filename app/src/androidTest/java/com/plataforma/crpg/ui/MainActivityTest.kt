0652d49162225bd263d4f75278c3dc6697542ee9
TiagoSantosDev <up201808919@fe.up.pt>
2021-05-14 19:04:02 +0100

    Adding imports to run more tests.

---
+++ app/src/androidTest/java/com/plataforma/crpg/ui/MainActivityTest.kt
@@ -0,0 +1,29 @@
+package com.plataforma.crpg.ui
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityUITest {
+
+    /*
+    @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
+        = new ActivityScenarioRule<>(MainActivity.class)
+    */
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun performActionWithVoiceCommand() {
+    }
+}
