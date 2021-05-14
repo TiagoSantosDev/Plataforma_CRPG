b275b95eb6b343ec51eefa14da78bc46aa541acc
TiagoSantosDev <up201808919@fe.up.pt>
2021-05-15 00:37:04 +0100

    Changed withText to withID, but tests crash on startup.

---
+++ app/src/androidTest/java/com/plataforma/crpg/ui/agenda/DatePickerFragmentTest.kt
@@ -0,0 +1,33 @@
+package com.plataforma.crpg.ui.agenda
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.plataforma.crpg.R
+import com.plataforma.crpg.ui.MainActivity
+import com.plataforma.crpg.ui.reminders.ReminderFragment
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class DatePickerFragmentTest{
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun launchFragmentAndVerifyUI(){
+        launchFragmentInContainer<DatePickerFragment>()
+
+        Espresso.onView(ViewMatchers.withId(R.id.selecionar_dia_hint)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.selecionar_dia_hint)).check(ViewAssertions.matches(ViewMatchers.withText("MOVA OS CARTÕES LARANJA PARA A ESQUERDA/DIREITA PARA VER MAIS DIAS!")))
+
+    }
+
+}
--- app/src/androidTest/java/com/plataforma/crpg/ui/reminders/ReminderFragmentTest.kt
+++ app/src/androidTest/java/com/plataforma/crpg/ui/reminders/ReminderFragmentTest.kt
@@ -2,9 +2,9 @@ package com.plataforma.crpg.ui.reminders
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
@@ -13,27 +13,35 @@ import kotlinx.android.synthetic.main.reminder_activity_intro.view.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import com.plataforma.crpg.R
 
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class ReminderFragmentTest {
 
+    /*
     @Rule @JvmField
     var main2 = ActivityScenarioRule(MainActivity::class.java)
+    */
 
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+/*
     @Rule @JvmField
     var main = ActivityTestRule(MainActivity::class.java)
-
+*/
 
     @Test
     fun launchFragmentAndVerifyUI(){
         launchFragmentInContainer<ReminderFragment>()
 
-        /*
-        onView(R.id.adicione_lembretes_hint).check(matches(withText(R.string.adicione_lembretes_hint)))
-        onView(R.id.clique_icone_lapis_hint).check(matches(withText(R.string.clique_icone_lapis_hint)))
-        onView(R.id.createReminderActionButton).check(matches(withText(R.string.adicione_lembretes_hint)))
-        */
+        onView(withId(R.id.adicione_lembretes_hint)).check(matches(withText("Adicione lembretes para não se esquecer do que é realmente importante!")))
+        //onView(R.id.adicione_lembretes_hint).check(matches(withText(R.string.adicione_lembretes_hint)))
+        //onView(R.id.clique_icone_lapis_hint).check(matches(withText(R.string.clique_icone_lapis_hint)))
+        onView(withId(R.id.createReminderActionButton)).perform(click())
+        onView(withId(R.id.createReminderActionButton)).check(matches(isDisplayed()))
+
     }
 
     @Test
