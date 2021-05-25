3697166d52b48a182191b762d22e576d3486fa53
TiagoSantosDev <up201808919@fe.up.pt>
2021-05-25 14:38:38 +0100

    Created notifications handler class.

---
+++ app/src/main/java/com/plataforma/crpg/services/NotificationsHandler.kt
@@ -0,0 +1,66 @@
+package com.plataforma.crpg.services
+
+import android.app.IntentService
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.plataforma.crpg.R
+import com.plataforma.crpg.ui.MainActivity
+
+class NotificationsHandler(name: String?) : IntentService(name) {
+
+    val ACTION1 = "ACTION1"
+    val ACTION2 = "ACTION2"
+
+    override fun onHandleIntent(p0: Intent?) {
+        val action = p0?.action;
+        if (ACTION1 == action) {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                println("Entrou no intent")
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+                /*
+                val fragment: Fragment = TransportsSelectionFragment()
+                val fragmentManager: FragmentManager = supportFragmentManager
+                val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+                fragmentManager.popBackStack()
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()*/
+            }
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+            val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                val channel = NotificationChannel("YOUR_CHANNEL_ID",
+                        "YOUR_CHANNEL_NAME",
+                        NotificationManager.IMPORTANCE_DEFAULT)
+                channel.description = "YOUR_NOTIFICATION_CHANNEL_DESCRIPTION"
+                mNotificationManager.createNotificationChannel(channel)
+            }
+
+            val builder = NotificationCompat.Builder(this, "YOUR_CHANNEL_ID")
+                    .setSmallIcon(R.drawable.ic_notification_bus)
+                    .setContentTitle("Não se esqueça de apanhar o transporte!")
+                    .setContentText("Clique aqui para abrir a aplicação e ver os horários!")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+
+            with(NotificationManagerCompat.from(this)) {
+                println("Entrou no Notification Manager Compat")
+                // notificationId is a unique int for each notification that you must define
+                notify(1, builder.build())
+            }
+        } else if (ACTION2 == action) {
+            // do some other stuff...
+        } else {
+            throw IllegalArgumentException("Unsupported action: " + action);
+        }
+
+    }
+}
