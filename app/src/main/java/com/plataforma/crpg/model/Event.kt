4b3a01b6e295afd97d40d5c4f65e0b1f20e23bf4
TiagoSantosDev <up201808919@fe.up.pt>
2021-05-25 15:56:00 +0100

    Added more values to Meal Model in order to facilitate Main Activity fecthing data for the Notifications.

--- app/src/main/java/com/plataforma/crpg/model/Event.kt
+++ app/src/main/java/com/plataforma/crpg/model/Event.kt
@@ -11,6 +11,7 @@ data class Event(
         var date: String,
         val notes: String,
         var chosen_meal: String,
+        var isLunch: Boolean,
         var meal_int: Int
 ){
     override fun toString(): String {
